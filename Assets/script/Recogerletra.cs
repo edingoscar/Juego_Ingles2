0248d8cc3c3e568f07e88b33bd7a90dbf111e71f
Edingoscar <76409814@liceosd.edu.pe>
2025-02-19 02:14:34 -0500

    Initial Commit

--- Assets/script/Recogerletra.cs
+++ Assets/script/Recogerletra.cs
@@ -5,39 +5,48 @@ public class RecogerLetras : MonoBehaviour
     public GameObject letraActual;
     public float contador_tiempo = 0;
     public LanzarLetra lanzarLetra;
+    public LayerMask letraLayer;
+    public Transform deteccionPunto;
+    public float radioDeteccion = 0.5f;
+
 
     void Update()
     {
         contador_tiempo += Time.deltaTime;
-
+        Collider2D letraDetectada = Physics2D.OverlapCircle(deteccionPunto.position, radioDeteccion, letraLayer);
         //  Evitamos que la tecla E pueda soltar si ya lanzamos la letra
+        if (Input.GetKeyDown(KeyCode.E) && letraDetectada != null && contador_tiempo > 0.5f && letraActual == null)
+        {
+            print("Recoger letra");
+            letraActual = letraDetectada.gameObject;
+            letraActual.transform.SetParent(transform);
+            letraActual.GetComponent<SpriteRenderer>().sortingOrder = 2;
+            lanzarLetra.AsignarLetra(letraActual.transform);
+            contador_tiempo = 0;
+        }
+
+        // Soltar la letra
         if (Input.GetKeyDown(KeyCode.E) && contador_tiempo > 0.5 && letraActual != null)
         {
             print("Soltar letra");
-            if (letraActual.transform.parent == transform)
-            {
-                letraActual.transform.SetParent(null);
-                letraActual.transform.position = transform.position; 
-                letraActual.gameObject.SetActive(true);
-            }
-            letraActual = null; // Eliminamos la referencia
-            lanzarLetra.AsignarLetra(null); // También avisamos a LanzarLetra
-
+            letraActual.transform.SetParent(null);
+            letraActual.transform.position = transform.position;
+            letraActual.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            letraActual = null;
+            lanzarLetra.AsignarLetra(null);
             contador_tiempo = 0;
+        }
+
+        if (letraActual != null)
+        {
+            letraActual.transform.position = new Vector3(transform.position.x, transform.position.y + 0.4f, letraActual.transform.position.z);
         }
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnDrawGizmosSelected()
     {
-        // Solo podemos recoger una letra si realmente no tenemos ninguna
-        if (Input.GetKey(KeyCode.E) && other.CompareTag("Letra") && contador_tiempo > 0.5 && letraActual == null)
-        {
-            print("Recoger letra");
-            letraActual = other.gameObject;
-            letraActual.transform.SetParent(transform);
-            lanzarLetra.AsignarLetra(letraActual.transform);
-            letraActual.gameObject.SetActive(false);
-            contador_tiempo = 0;
-        }
+        // Dibujar el radio de detección en la escena para depuración
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(deteccionPunto.position, radioDeteccion);
     }
 }
--- Assets/script/camaraController.cs
+++ Assets/script/camaraController.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class camaraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform player;
+
+    public float limitLeft = -2;
+    public float limitRight = 2;
+    public float limitDown = -2;
+    public float limitUp = 2;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+            transform.position = new Vector3(Mathf.Clamp(player.transform.position.x,limitLeft,limitRight),
+                Mathf.Clamp(player.transform.position.y,limitDown,limitUp),transform.position.z);
     }
 }
